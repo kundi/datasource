625da02386f7290cc9c141aab71072fcbc1cb21d
Jan Berdajs <mrbrdo@gmail.com>
2015-02-04 20:14:28 +0100

    filter out models without id for collection context, provide all_models that is unfiltered

--- lib/datasource/collection_context.rb
+++ lib/datasource/collection_context.rb
@@ -1,18 +1,31 @@
 module Datasource
   class CollectionContext
-    attr_reader :scope, :models, :datasource, :datasource_class, :params, :loaded_values
+    attr_reader :scope, :all_models, :datasource, :datasource_class, :params, :loaded_values
 
     def initialize(scope, collection, datasource, params)
       @scope = scope
-      @models = collection
+      @all_models = collection
       @datasource = datasource
       @datasource_class = datasource.class
       @params = params
       @loaded_values = {}
     end
 
+    def models
+      return @models if @models
+
+      @model_ids = []
+      @models = all_models.select do |model|
+        id = model.send(@datasource_class.primary_key)
+        @model_ids << id
+        id
+      end
+    end
+
     def model_ids
-      @model_ids ||= @models.map(&@datasource_class.primary_key)
+      return @model_ids if @model_ids
+      models
+      @model_ids
     end
     alias_method :ids, :model_ids
   end
